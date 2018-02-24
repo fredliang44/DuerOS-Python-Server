6187d2ea1af298b109af30623da5bd57dd0cfe60
梁志博 <me@fredliang.cn>
2018-02-25 01:38:05 +0800

    Modify style

--- app/handler/Main.py
+++ app/handler/Main.py
@@ -13,16 +13,16 @@ class MainHandler:
             raise Exception("Unsupport %s data type!" % self.__class__.__name__)
 
     def get(self, key):
-        self.traversal_dict(key, self.data, all=True)
+        self.__traversal_dict(key, self.data, all=True)
         return self.__got_data
 
     def get_all(self, key):
-        self.traversal_dict(key, self.data, all=False)
+        self.__traversal_dict(key, self.data, all=False)
         return self.__got_all_data
 
     """traversal to search key"""
 
-    def traversal_dict(self, key, dictionary, all):
+    def __traversal_dict(self, key, dictionary, all):
         if isinstance(dictionary, dict):
             for x in range(len(dictionary)):
                 temp_key = list(dictionary.keys())[x]
@@ -34,6 +34,6 @@ class MainHandler:
                         self.__got_data = temp_value
                     return
 
-                self.traversal_dict(key, temp_value, all=all)
+                self.__traversal_dict(key, temp_value, all=all)
         else:
             return None
